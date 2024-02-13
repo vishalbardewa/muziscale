bd21c0065b8b54581b86a22bb5009fe3bf458943
Vishal Bardewa <bardewavishal@gmail.com>
2024-02-13 16:56:53 +0530

    Updated meta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
-import Head from 'next/head';
+import Head from "next/head";
 
-import { CallToAction } from '@/components/CallToAction';
-import Explore from '@/components/Explore';
-import { Faqs } from '@/components/Faqs';
-import { Footer } from '@/components/Footer';
-import Header from '@/components/Header';
-import { Hero } from '@/components/Hero';
-import { Pricing } from '@/components/Pricing';
-import { SecondaryFeatures } from '@/components/SecondaryFeatures';
-import { Testimonials } from '@/components/Testimonials';
+import { CallToAction } from "@/components/CallToAction";
+import Explore from "@/components/Explore";
+import { Faqs } from "@/components/Faqs";
+import { Footer } from "@/components/Footer";
+import Header from "@/components/Header";
+import { Hero } from "@/components/Hero";
+import { Pricing } from "@/components/Pricing";
+import { SecondaryFeatures } from "@/components/SecondaryFeatures";
+import { Testimonials } from "@/components/Testimonials";
 
 export default function Home() {
   return (
@@ -19,6 +19,10 @@ export default function Home() {
           name="Muziscale - Become a professional musician in 3 months"
           content="Transform your passion into expertise with Muziscale! Elevate your musical journey in just 3 months and become a professional musician. Unleash your potential now!"
         />
+        <meta name="robots" content="index,follow"></meta>
+        <meta property="og:title" content="Muziscale - Become a professional musician in 3 months"></meta>
+        <meta property="og:site_name" content="Muziscale"></meta>
+        <meta property="og:locale" content="en"></meta>
       </Head>
       <Header />
       <main>
